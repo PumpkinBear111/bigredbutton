1d3747b0cfbf840b4be28becee4ffdf787557037
PumpkinBear111 <unconfigured@null.spigotmc.org>
2021-03-19 12:49:51 -0700

    Added a basic blue button block and item - no functionality yet.

---
+++ src/main/java/me/pumpkinbear111/bigredbutton/blocks/blue_button.java
@@ -0,0 +1,41 @@
+package me.pumpkinbear111.bigredbutton.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
+import net.minecraftforge.common.ToolType;
+
+import java.util.stream.Stream;
+
+public class blue_button extends Block {
+
+    public static final DirectionProperty FACING = DirectionProperty.create(Direction.NORTH.getName());
+
+    public blue_button() {
+        super(Properties.of(Material.STONE)
+                .strength(5, 0)
+                .sound(SoundType.STONE)
+                .requiresCorrectToolForDrops()
+                .harvestTool(ToolType.PICKAXE)
+                .noOcclusion());
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPE_N;
+    }
+
+    private static final VoxelShape SHAPE_N = Stream.of(
+            Block.box(1, 0, 1, 15, 4, 15)
+    ).reduce((v1, v2) -> {return VoxelShapes.join(v1, v2, IBooleanFunction.OR);}).get();
+
+}
